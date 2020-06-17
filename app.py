f2ec17307c1a9ce31307ce1c0c0669a9ded36330
Penrose Learning <50510199+penroselearning@users.noreply.github.com>
2020-06-17 19:02:41 +0400

    App V2

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,request
 from flask import session
-#from flask_restful import Resource, Api
 from calculation import calc_square,convert_to_seconds,birthday_countdown,aspect_ratio_calculator
 from word_cookies import find_words,find_vowels
 from wild_cats import wild_cats
@@ -18,6 +17,7 @@ def index():
 def about():
     return render_template("about.html")
 
+# Numbers
 @app.route("/square",methods=["GET", "POST"])
 def square():
     errors = ""
@@ -35,29 +35,101 @@ def square():
             errors += f'Please Enter a Number'
     return render_template("square.html",solution=result, error_msg=errors)
 
-@app.route("/moon_phase_calculator",methods=["GET", "POST"])
-def moon_phase_calculator_func():
+# For Loop
+@app.route("/multiplication_table",methods=["GET","POST"])
+def multiplicationtable():
     errors = ""
-    result = ""
-    next_full = ""
-    next_new = ""
+    number = None
+    if request.method == "POST":
+        if request.form["number"] is not None:
+            try:
+                number = int(request.form["number"])
+            except:
+                errors += f'Please Enter a Number'
+        else:
+            errors += f'Please Enter a Number'
 
-    next_full = nextphasefm()
-    next_new = nextphasenm()
+    return render_template("multiplication_table.html",n=number, error_msg=errors)
+
+@app.route("/guessing_game",methods=["GET","POST"])
+def guessinggame():
+    
+    result =""
+    errors =""
+    guess = ""
+    
+    if request.method == "POST":
+        try:
+            guess=int(request.form["number"])
+            result = guessing_game(guess)
+        except:
+            guess=0
+    
+    return render_template("guessing_game.html",game=result,error_msg=errors)
+
+# List
+@app.route("/shopping_list",methods=["GET","POST"])
+def shoppinglist():
+    
+    result =""
+    errors =""
+    list=""
+    remove=""
+    
+    if request.method == "POST":
+        try:
+            item = request.form["item"]
+        except:
+            remove = request.form["remove"]
+        
+        if remove == "":
+            list = shopping_cart(item)
+        else:
+            list = shopping_cart_remove(remove)
+        
+    return render_template("shopping_list.html",cart=list,error_msg=errors)
+
+# Dictionary
+@app.route("/find_vowels",methods=["GET", "POST"])
+def findvowels():
+    errors = ""
+    result = {}
+    word = ""
+    vowels={'a':0,'e':0,'i':0,'o':0,'u':0}
+
+    with app.open_resource('static/engmix.txt') as file:
+        wordlist = [x.strip().decode() for x in file.readlines()]
+    
+    if request.method == "POST":
+        if request.form["text"] is not None:
+            word = request.form["text"]
+            if word in wordlist:
+                result = find_vowels(word,vowels)
+            else:
+                errors += f'This Word does not exist in a dictionary'
+        else:
+            errors += f'Please Enter a Word'
+    
+    return render_template("find_vowels.html",solution=result, error_msg=errors)
+
+# Combining Lists and Dictionaries
+@app.route("/wild_cats",methods=["GET", "POST"])
+def wildcats():
+    errors = ""
+    scat = ''
+    
+    wcats = wild_cats()
 
     if request.method == "POST":
-        selecteddate = request.form["date"]
-        
-        if selecteddate !='':
-            result = specificdate(selecteddate)
-        else:
-            errors += f'Please select a Date'
-                
-    return render_template("moon_phase_calculator.html",moonphase=result,nextfull=next_full, nextnew = next_new, error_msg=errors)
+        scat = request.form["cats"]
+    
+    #result = wild_cats(wild_cats)
 
+    return render_template("wild_cats.html",cats=wcats,cat=scat, error_msg=errors)
 
+# Date and Time
 @app.route("/birthday_countdown",methods=["GET", "POST"])
-def birthday_countdown_func():
+def birthdaycountdown():
     errors=""
     result = None
     if request.method == "POST":
@@ -72,23 +144,6 @@ def birthday_countdown_func():
                 result = birthday_countdown(birthdate)
     
     return render_template("birthday_countdown.html",countdown=result,error_msg=errors)
-
-
-
-@app.route("/multiplication_table",methods=["GET","POST"])
-def mtable():
-    errors = ""
-    number = None
-    if request.method == "POST":
-        if request.form["number"] is not None:
-            try:
-                number = int(request.form["number"])
-            except:
-                errors += f'Please Enter a Number'
-        else:
-            errors += f'Please Enter a Number'
-
-    return render_template("multiplication_table.html",n=number, error_msg=errors)
 
 @app.route("/convert_to_seconds",methods=["GET","POST"])
 def converttoseconds():
@@ -126,7 +181,7 @@ def converttoseconds():
     result = convert_to_seconds(y,mo,w,d,h,m)
     return render_template("convert_to_seconds.html",vals=(y,mo,w,d,h,m),solution=f'{result:,}', error_msg=errors)
 
-
+# Build Functions
 @app.route("/aspect_ratio_calculator",methods=["GET","POST"])
 def aspectratiocalculator():
     d=""
@@ -158,6 +213,25 @@ def aspectratiocalculator():
     d = aspect_ratio_calculator(w,h,nw,nh)
     return render_template("aspect_ratio_calculator.html",s=d, error_msg=errors)
 
+@app.route("/moon_phase_calculator",methods=["GET", "POST"])
+def moonphasecalculator():
+    errors = ""
+    result = ""
+    next_full = ""
+    next_new = ""
+
+    next_full = nextphasefm()
+    next_new = nextphasenm()
+
+    if request.method == "POST":
+        selecteddate = request.form["date"]
+        
+        if selecteddate !='':
+            result = specificdate(selecteddate)
+        else:
+            errors += f'Please select a Date'
+                
+    return render_template("moon_phase_calculator.html",moonphase=result,nextfull=next_full, nextnew = next_new, error_msg=errors)
 
 @app.route("/word_cookies_cheat",methods=["GET", "POST"])
 def word_cookies_cheat():
@@ -176,81 +250,6 @@ def word_cookies_cheat():
             errors += f'Please Enter Letters'
     
     return render_template("word_cookies_cheat.html",solution=result, error_msg=errors)
-
-@app.route("/find_vowels",methods=["GET", "POST"])
-def find_vowels_app():
-    errors = ""
-    result = {}
-    word = ""
-    vowels={'a':0,'e':0,'i':0,'o':0,'u':0}
-
-    with app.open_resource('static/engmix.txt') as file:
-        wordlist = [x.strip().decode() for x in file.readlines()]
-    
-    if request.method == "POST":
-        if request.form["text"] is not None:
-            word = request.form["text"]
-            if word in wordlist:
-                result = find_vowels(word,vowels)
-            else:
-                errors += f'This Word does not exist in a dictionary'
-        else:
-            errors += f'Please Enter a Word'
-    
-    return render_template("find_vowels.html",solution=result, error_msg=errors)
-
-@app.route("/wild_cats",methods=["GET", "POST"])
-def wild_cats_app():
-    errors = ""
-    scat = ''
-    
-    wcats = wild_cats()
-
-    if request.method == "POST":
-        scat = request.form["cats"]
-    
-    #result = wild_cats(wild_cats)
-
-    return render_template("wild_cats.html",cats=wcats,cat=scat, error_msg=errors)
-
-
-@app.route("/guessing_game",methods=["GET","POST"])
-def guessinggame():
-    
-    result =""
-    errors =""
-    guess = ""
-    
-    if request.method == "POST":
-        try:
-            guess=int(request.form["number"])
-            result = guessing_game(guess)
-        except:
-            guess=0
-    
-    return render_template("guessing_game.html",game=result,error_msg=errors)
-
-
-@app.route("/shopping_list",methods=["GET","POST"])
-def shoppinglist():
-    
-    result =""
-    errors =""
-    list=""
-    remove=""
-    
-    if request.method == "POST":
-        try:
-            item = request.form["item"]
-        except:
-            remove = request.form["remove"]
-        
-        if remove == "":
-            list = shopping_cart(item)
-        else:
-            list = shopping_cart_remove(remove)
-        
-    return render_template("shopping_list.html",cart=list,error_msg=errors)
 
 
 if __name__ == '__main__':
